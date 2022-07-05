46e0ff18cb7da1490f46d6b45512cdd1cd0332e2
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-07-05 16:19:55 -0300

    chore: construction src

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./src/routes/rickandmorty.route");
-
+const connectToDatabase = require("./src/database/mongoConnection");
 
 const port = 3000;
 const app = express();
 
 app.use(express.json()); 
 app.use(cors()); 
-
-app.use("/rickandmorty", routes);
+connectToDatabase();
+app.use("/personagens", routes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
