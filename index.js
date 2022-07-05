4f1ec1e507af87fecada95a2102db8b6e4e8c89a
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-07-05 16:08:37 -0300

    chore: initial config

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const routes = require("./src/routes/rickandmorty.route");
+
+
+const port = 3000;
+const app = express();
+
+app.use(express.json()); 
+app.use(cors()); 
+
+app.use("/rickandmorty", routes);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
