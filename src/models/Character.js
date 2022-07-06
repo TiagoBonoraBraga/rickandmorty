49783df42a0c9c77e160b01b0b9823670d880ceb
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-07-05 21:53:07 -0300

    characters

---
+++ src/models/Character.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const CharacterSchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    required: true,
+  },
+  foto: {
+    type: String,
+    required: true,
+  },
+});
+
+const Character = mongoose.model('characters', CharacterSchema);
+module.exports = Character;
--- src/routes/character.route.js
+++ src/routes/character.route.js
@@ -1,13 +1,12 @@
+const route = require('express').Router();
+const controllerCharacters = require('../controllers/character.controller');
 
-const route = require("express").Router();
-const controllerCharacters = require("../controllers/character.controller");
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('../../swagger.json');
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../../swagger.json');
+// route.use('/api-docs', swaggerUi.serve);
+// route.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-route.use('/api-docs', swaggerUi.serve);
-route.get('/api-docs', swaggerUi.setup(swaggerDocument));
-
-route.get("/", controllerCharacters.findAllCharactersController);
+route.get('/', controllerCharacters.findAllCharactersController);
 
 module.exports = route;
