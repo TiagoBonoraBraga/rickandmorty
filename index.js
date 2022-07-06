b02acc53ec627e3e9c8e838f5a68f5308ecd1d1f
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-07-05 21:21:07 -0300

    chore: mongoatlas e dotenv config

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const routes = require("./src/routes/character.route");
-const connectToDatabase = require("./src/database/database");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const routes = require('./src/routes/character.route');
+const connectToDatabase = require('./src/database/database');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.json()); 
-app.use(cors()); 
+app.use(express.json());
+app.use(cors());
 connectToDatabase();
-app.use("/characters", routes);
+app.use('/characters', routes);
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
